e211f1b7d616f7b14d68374ac2e98d0eaeaa86dd
SewerCzar <86573170+SewerCzar@users.noreply.github.com>
2021-10-25 14:16:34 -0400

    Gem Push (#992)
    * Gem Push

---
+++ Source/Kesmai.Server/Game/Items/Gems/SmallBlackPearl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Kesmai.Server.Network;
+
+namespace Kesmai.Server.Items
+{
+	public partial class SmallBlackPearl : Gem
+	{
+		/// <inheritdoc />
+		public override int Weight => 5;
+
+		/// <summary>
+		/// Gets the label number.
+		/// </summary>
+		public override int LabelNumber => 6000069;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SmallBlackPearl"/> class.
+		/// </summary>
+		[WorldForge]
+		public SmallBlackPearl(uint basePrice) : base(347, basePrice)
+		{
+		}
+
+		/// <inheritdoc />
+		public override void GetDescription(List<LocalizationEntry> entries)
+		{
+			entries.Add(new LocalizationEntry(6200000, 6200292)); /* [You are looking at] [a small black pearl.] */
+		}
+	}
+}
