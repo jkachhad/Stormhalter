jkachhad/Stormhalter

commit 48925a72cf5d28e8f8001a83fde2a32dcd627caa

Delete hotkey behavior respects current filter
pressing Delete now only clears components shown by the current Filter. Also triggers a new render pass to reflect changes immediately.

Source/Kesmai.WorldForge/Game/Screens/WorldGraphicsScreen.cs
@@ -495,9 +495,17 @@ namespace Kesmai.WorldForge
 						{
 							for (var x = area.Left; x < area.Right; x++)
 								for (var y = area.Top; y < area.Bottom; y++)
-									region.DeleteTile(x, y);
-						}
+								{
+									var currentFilter = _presenter.SelectedFilter;
+									var tile = region.GetTile(x,y);
+									var validComponents = tile.Components.Where(c => currentFilter.IsValid(c)).ToArray();
+									foreach (var component in validComponents){
+										tile.RemoveComponent(component);
+									}
+								}
 
+						}
+						_invalidateRender = true;
 						inputService.IsKeyboardHandled = true;
 					}
 				}

commit 9dee77d7d604a52a2024a730a48631ee21a29729

Fix for deleting a selection with empty tiles.
Fixes bug introduced in 48925a7: when the current selection includes a tile without data, the delete action would encounter a NullReferenceException.

Source/Kesmai.WorldForge/Game/Screens/WorldGraphicsScreen.cs
@@ -498,6 +498,8 @@ namespace Kesmai.WorldForge
 								{
 									var currentFilter = _presenter.SelectedFilter;
 									var tile = region.GetTile(x,y);
+									if (tile is null)
+										continue;
 									var validComponents = tile.Components.Where(c => currentFilter.IsValid(c)).ToArray();
 									foreach (var component in validComponents){
 										tile.RemoveComponent(component);