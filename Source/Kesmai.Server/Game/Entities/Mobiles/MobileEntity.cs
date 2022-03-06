5a624710d2e8b8b2decf6815953fcbc8bdb152d1
Jalpesh Kachhadia <jalpeshk@gmail.com>
2022-03-05 22:10:38 -0800

    CalculateDamageModifier

---
+++ Source/Kesmai.Server/Game/Entities/Mobiles/MobileEntity.cs
@@ -0,0 +1,30 @@
+namespace Kesmai.Server.Game
+{
+	public abstract partial class MobileEntity : WorldEntity
+	{
+		/// <summary>
+		/// Calculates the damage modifier against the specified item.
+		/// </summary>
+		public int CalculateDamageModifier(ItemEntity item)
+		{
+			var paperdoll = Paperdoll;
+			
+			if (paperdoll is null)
+				return 0;
+			
+			var damageModifier = 0;
+			
+			var armor = paperdoll.Armor;
+
+			if (armor != null)
+			{
+				var armorBonus = armor.GetArmorBonus(item);
+
+				if (armorBonus > 3)
+					damageModifier = armorBonus - 3;
+			}
+
+			return damageModifier;
+		}
+	}
+}
