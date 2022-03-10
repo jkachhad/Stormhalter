1613065837c3f5d2435caae51409e0eec9736d70
Jalpesh Kachhadia <jalpeshk@gmail.com>
2022-03-09 20:15:46 -0800

    Duration of all Resistance and Protection spells have been doubled.

---
+++ Source/Kesmai.Server/Spells/Spell.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Kesmai.Server.Spells
+{
+	public abstract partial class Spell
+	{
+		public static double ResistanceSpellDurationMultiplier = 2.0;
+		public static double ProtectionSpellDurationMultiplier = 2.0;
+	}
+}
