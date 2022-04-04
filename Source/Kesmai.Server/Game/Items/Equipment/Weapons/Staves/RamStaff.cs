f0ff3ff0ab66977d0672fd3c5188273680dd88a0
Chilly021 <chilly021@gmail.com>
2022-04-03 22:12:03 -0500

    Add Ram Skull Staff

---
+++ Source/Kesmai.Server/Game/Items/Equipment/Weapons/Staves/RamStaff.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.IO;
+using Kesmai.Server.Game;
+using Kesmai.Server.Network;
+
+namespace Kesmai.Server.Items
+{
+	public partial class RamStaff : Staff
+	{
+        /// <inheritdoc />
+        public override int LabelNumber => 6000088;
+
+		/// <inheritdoc />
+		public override uint BasePrice => 800;
+
+		/// <inheritdoc />
+		public override int Weight => 1400;
+
+        public override ShieldPenetration Penetration => ShieldPenetration.Medium;
+
+		/// <inheritdoc />
+		public override int Category => 1;
+
+		/// <inheritdoc />
+		public override int MinimumDamage => 1;
+
+		/// <inheritdoc />
+		public override int MaximumDamage => 8;
+
+		/// <inheritdoc />
+		public override int BaseArmorBonus => 4;
+
+		/// <inheritdoc />
+		public override int BaseAttackBonus => 2;
+
+		/// <inheritdoc />
+		public override WeaponFlags Flags => WeaponFlags.TwoHanded | WeaponFlags.Bashing;
+		
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BlackStaff"/> class.
+		/// </summary>
+		public BlackStaff() : base(901)
+		{
+		}
+		
+		/// <inheritdoc />
+		public override void GetDescription(List<LocalizationEntry> entries)
+		{
+			entries.Add(new LocalizationEntry(6200000, 6200353)); /* [You are looking at] [a staff large gnarled staff with a ram skull attached.] */
+
+            if (Identified)
+				entries.Add(new LocalizationEntry(6250080)); /* The combat adds for this weapon are +1. */
+		}
+	}
+}
