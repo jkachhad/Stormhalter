6680e0795e260b80a7896f11d1b46c39a40a9186
Chilly021 <90510109+Chilly021@users.noreply.github.com>
2022-09-15 15:52:08 -0500

    Update Unholy Scepter.
    Forgot to format the file with indentation.

--- Source/Kesmai.Server/Game/Items/Equipment/Weapons/Maces/UnholyScepter.cs
+++ Source/Kesmai.Server/Game/Items/Equipment/Weapons/Maces/UnholyScepter.cs
@@ -8,66 +8,66 @@ namespace Kesmai.Server.Items
     public partial class UnholyScepter : Mace
     {
 
-		/// <inheritdoc />
-		public override uint BasePrice => 10000;
+        /// <inheritdoc />
+        public override uint BasePrice => 10000;
 
-		/// <inheritdoc />
-		public override int Weight => 2000;
+        /// <inheritdoc />
+        public override int Weight => 2000;
 
-		/// <inheritdoc />
-		public override int Category => 2;
+        /// <inheritdoc />
+        public override int Category => 2;
 
-		/// <inheritdoc />
-		public override int MinimumDamage => 2;
+        /// <inheritdoc />
+        public override int MinimumDamage => 2;
 
-		/// <inheritdoc />
-		public override int MaximumDamage => 10;
+        /// <inheritdoc />
+        public override int MaximumDamage => 10;
 
         /// <inheritdoc />
         public override ShieldPenetration Penetration => ShieldPenetration.Light;
 
-		/// <inheritdoc />
-		public override int BaseArmorBonus => 3;
+        /// <inheritdoc />
+        public override int BaseArmorBonus => 3;
 
         /// <inheritdoc />
         public override int BaseAttackBonus => 3;
 
-         /// <inheritdoc />
+        /// <inheritdoc />
         public override int ManaRegeneration => 1;
 
-		/// <inheritdoc />
-		public override Skill Skill => Skill.Mace;
+        /// <inheritdoc />
+        public override Skill Skill => Skill.Mace;
 
-		/// <inheritdoc />
+        /// <inheritdoc />
         public override WeaponFlags Flags => WeaponFlags.Bashing | WeaponFlags.BlueGlowing | WeaponFlags.Lawful;
 
         /// <inheritdoc />
 		public override bool CanBind => true;
-		
-		/// <summary>
-		/// Initializes a new instance of the <see cref="UnholyScepter"/> class.
-		/// </summary>
-		public UnholyScepter() : base(965)
-		{
-		}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnholyScepter"/> class.
+        /// </summary>
+        public UnholyScepter() : base(965)
+        {
+        }
 
         /// <inheritdoc />
 		public override bool CanEquip(MobileEntity entity)
-		{
-			if (entity is PlayerEntity { Profession: Profession.Thaumaturge } || entity is PlayerEntity { Profession: Profession.Wizard })
-				return true;
+        {
+            if (entity is PlayerEntity { Profession: Profession.Thaumaturge } || entity is PlayerEntity { Profession: Profession.Wizard })
+                return true;
 
-			return false;
-		}
+            return false;
+        }
 
-		/// <inheritdoc />
-		public override void GetDescription(List<LocalizationEntry> entries)
-		{
-			entries.Add(new LocalizationEntry(6200000, 6200382)); /* [You are looking at] [an unholy scepter that has the markings of a king. The weapon emanates power. The weapon is lawful.] */
+        /// <inheritdoc />
+        public override void GetDescription(List<LocalizationEntry> entries)
+        {
+            entries.Add(new LocalizationEntry(6200000, 6200382)); /* [You are looking at] [an unholy scepter that has the markings of a king. The weapon emanates power. The weapon is lawful.] */
 
-			if (Identified)
-				entries.Add(new LocalizationEntry(6250134)); /* The scepter seems to have some magical properties. */
-		}
+            if (Identified)
+                entries.Add(new LocalizationEntry(6250134)); /* The scepter seems to have some magical properties. */
+        }
     }
 
 
