99bdb9302c4c814fa65c275fc6a8a534c5ae852f
Chilly021 <90510109+Chilly021@users.noreply.github.com>
2023-04-21 20:14:11 -0500

    Update Polearm

--- Source/Kesmai.Server/Game/Items/Equipment/Weapons/Halberds/Polearm.cs
+++ Source/Kesmai.Server/Game/Items/Equipment/Weapons/Halberds/Polearm.cs
@@ -31,7 +31,7 @@ public class Polearm : Halberd, ITreasure
         public override bool CanBind => true;
 
         /// <inheritdoc />
-		public override WeaponFlags Flags => WeaponFlags.Slashing | WeaponFlags.Slashing | weaponFlags.Pierce | WeaponFlags.BlueGlowing;
+		public override WeaponFlags Flags => WeaponFlags.Slashing | WeaponFlags.Slashing | WeaponFlags.Pierce | WeaponFlags.BlueGlowing;
 
         private int _weaponLevel;
         private poleArmWeapon _weaponStats;
