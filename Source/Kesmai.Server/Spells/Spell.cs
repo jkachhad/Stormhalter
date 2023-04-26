aa73924048631ee7cd1b8cf8a27284f0406765af
Tenscore <97433823+Tenscore@users.noreply.github.com>
2023-04-26 11:50:32 -0500

    Update Spell.cs
    should be around 5minutes.
    No way to be sure but to test!

--- Source/Kesmai.Server/Spells/Spell.cs
+++ Source/Kesmai.Server/Spells/Spell.cs
@@ -4,7 +4,7 @@ namespace Kesmai.Server.Spells
 {
 	public abstract partial class Spell
 	{
-		public static double ResistanceSpellDurationMultiplier = 2.0;
-		public static double ProtectionSpellDurationMultiplier = 2.0;
+		public static double ResistanceSpellDurationMultiplier = 5.0;
+		public static double ProtectionSpellDurationMultiplier = 5.0;
 	}
 }
