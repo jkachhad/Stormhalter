a83b6d1152b58ef8f4f529d0a821cc32b17f9669
jkachhad <jalpeshk@gmail.com>
2023-09-03 13:53:45 -0700

    Add ActionTrap

---
+++ Source/Kesmai.Server/Game/World/Maps/Components/Functional/ActionTrap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml.Linq;
+using Kesmai.Server.Miscellaneous.WorldForge;
+
+namespace Kesmai.Server.Game;
+
+[WorldForgeComponent("ActionTrap")]
+public class ActionTrap : TrapComponent
+{
+	private string _action;
+	private Action<MobileEntity> Action;
+	
+	public ActionTrap(XElement element) : base(element)
+	{
+		if (element.TryGetElement("action", out var actionElement))
+			_action = (string)actionElement;
+	}
+
+	public override void Initialize()
+	{
+		base.Initialize();
+
+		var parent = Parent;
+
+		if (parent is null || String.IsNullOrEmpty(_action))
+			return;
+		
+		var internalCache = parent.Segment.Cache.Internal;
+		var actionMethod = internalCache.GetMethod(_action);
+
+		if (actionMethod != null)
+			Action = actionMethod.CreateDelegate<Action<MobileEntity>>(null);
+	}
+
+	protected override void OnSpring(MobileEntity entity)
+	{
+		if (Action != null)
+			Action(entity);
+	}
+}
---
+++ Source/Kesmai.WorldForge/Editor/MVP/Models/World/Components/Functional/ActionTrap.cs
@@ -0,0 +1,40 @@
+using System;
+using System.ComponentModel;
+using System.Xml.Linq;
+
+namespace Kesmai.WorldForge.Models;
+
+public class ActionTrap : TrapComponent
+{
+	private string _action;
+	
+	[Browsable(true)]
+	public string Action
+	{
+		get => _action;
+		set => _action = value;
+	}
+	
+	public ActionTrap(XElement element) : base(element)
+	{
+		var damageElement = element.Element("action");
+
+		if (damageElement != null)
+			_action = (string)damageElement;
+	}
+	
+	public override XElement GetXElement()
+	{
+		var element = base.GetXElement();
+
+		if (!String.IsNullOrEmpty(_action))
+			element.Add(new XElement("action", _action));
+		
+		return element;
+	}
+	
+	public override TerrainComponent Clone()
+	{
+		return new ActionTrap(GetXElement());
+	}
+}
