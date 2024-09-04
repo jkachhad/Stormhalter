e07f542b9d632e641f4f42029213d9ebf890ed03
jkachhad <jalpeshk@gmail.com>
2024-09-04 14:30:28 -0700

    Remove deprecated ShieldItemSource

--- Source/Kesmai.Server/Game/Items/Equipment/Rings/Shield/ShieldRing.cs
+++ Source/Kesmai.Server/Game/Items/Equipment/Rings/Shield/ShieldRing.cs
@@ -60,13 +60,13 @@ public partial class ShieldRing : Ring, ITreasure
 			{
 				Inscription = new SpellInscription() { SpellId = 52 }
 			};
-			status.AddSource(new ShieldStatus.ShieldItemSource(Shield, this));
+			status.AddSource(new ItemSource(this));
 				
 			entity.AddStatus(status);
 		}
 		else
 		{
-			status.AddSource(new ShieldStatus.ShieldItemSource(Shield, this));
+			status.AddSource(new ItemSource(this));
 		}
 
 		entity.Stats[EntityStat.Barrier].Add(+Shield, ModifierType.Constant);
