addc8627d5bedfd8b30ac8a855c69609ef423b7a
jkachhad <jalpeshk@gmail.com>
2025-07-19 15:39:55 -0700

    Add ClearcastStatus

---
+++ Source/Kesmai.Server/Spells/Status/ClearcastStatus.cs
@@ -0,0 +1,44 @@
+﻿using Kesmai.Server.Game;
+
+namespace Kesmai.Server.Spells;
+
+public class ClearcastStatus : SpellStatus
+{
+	public override int SpellRemovedSound => 221;
+	
+	public override bool Hidden => true;
+	
+	private Timer _internalTimer;
+
+	public ClearcastStatus(MobileEntity entity) : base(entity)
+	{
+	}
+	
+	public override void OnAcquire()
+	{
+		Refresh();
+	}
+		
+	public override void OnRemoved()
+	{
+		if (_internalTimer != null && _internalTimer.Running)
+			_internalTimer.Stop();
+
+		_internalTimer = null;
+			
+		base.OnRemoved();
+	}
+		
+	public void Refresh()
+	{
+		if (_internalTimer != null && _internalTimer.Running)
+			_internalTimer.Stop();
+			
+		_internalTimer = Timer.DelayCall(_entity.Facet.TimeSpan.FromRounds(10.0), OnTick);
+	}
+		
+	private void OnTick()
+	{
+		_entity.RemoveStatus(this);
+	}
+}
