f84d7eb470ab7250d56628682ebccb691de74734
jkachhad <jalpeshk@gmail.com>
2025-07-19 16:18:11 -0700

    Add Clearcast spell message and related audio notifications.

--- Source/Kesmai.Server/Spells/Status/ClearcastStatus.cs
+++ Source/Kesmai.Server/Spells/Status/ClearcastStatus.cs
@@ -1,22 +1,31 @@
-﻿using Kesmai.Server.Game;
+﻿using System.Drawing;
+using Kesmai.Server.Game;
 
 namespace Kesmai.Server.Spells;
 
 public class ClearcastStatus : SpellStatus
 {
 	public override int SpellRemovedSound => 221;
-	
-	public override bool Hidden => true;
-	
+
 	private Timer _internalTimer;
 
 	public ClearcastStatus(MobileEntity entity) : base(entity)
 	{
+		Inscription = new SpellInscription()
+		{
+			SpellId = 112,
+		};
 	}
 	
 	public override void OnAcquire()
 	{
 		Refresh();
+
+		if (_entity.Client != null)
+		{
+			_entity.PlaySound(10004);
+			_entity.SendCombatMessage(Color.Magenta, 6300433); /* You gain Clearcast. */
+		}
 	}
 		
 	public override void OnRemoved()
@@ -25,6 +34,9 @@ public class ClearcastStatus : SpellStatus
 			_internalTimer.Stop();
 
 		_internalTimer = null;
+		
+		if (_entity.Client != null)
+			_entity.SendCombatMessage(Color.Magenta, 6300434); /* You lost Clearcast. */
 			
 		base.OnRemoved();
 	}
