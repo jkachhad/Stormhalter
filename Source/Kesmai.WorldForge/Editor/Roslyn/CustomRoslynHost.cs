9e23cca2705e0277c4fd60d6e5319d39c364e9ac
jkachhad <jalpeshk@gmail.com>
2025-09-01 17:55:33 -0700

    Cleanup

--- Source/Kesmai.WorldForge/Editor/Roslyn/CustomRoslynHost.cs
+++ Source/Kesmai.WorldForge/Editor/Roslyn/CustomRoslynHost.cs
@@ -58,59 +58,36 @@ public class CustomRoslynHost : RoslynHost
     
     protected override Project CreateProject(Solution solution, DocumentCreationArgs args, CompilationOptions compilationOptions, Project? previousProject = null)
     {
-        var workspace = solution.Workspace;
-        
-        var name = args.Name ?? "New";
-        var path = Path.Combine(args.WorkingDirectory, name);
-        var id = ProjectId.CreateNewId(name);
+        var projectName = "Editor";
+        var projectId = ProjectId.CreateNewId(projectName);
 
         var parseOptions = ParseOptions.WithKind(args.SourceCodeKind);
-        var isScript = args.SourceCodeKind == SourceCodeKind.Script;
-
-        if (isScript)
-        {
-            compilationOptions = compilationOptions.WithScriptClassName(name);
-        }
-
-        var analyzerConfigDocuments = AnalyzerConfigFiles.Where(File.Exists).Select(file => DocumentInfo.Create(
-            DocumentId.CreateNewId(id, debugName: file),
-            name: file,
-            loader: new FileTextLoader(file, defaultEncoding: null),
-            filePath: file));
-
+        
         var projectReferences = solution.ProjectIds.Select(p => new ProjectReference(p));
         
         solution = solution.AddProject(ProjectInfo.Create(
-                id,
-                VersionStamp.Create(),
-                name,
-                name,
+                projectId, VersionStamp.Create(), projectName, projectName,
                 LanguageNames.CSharp,
-                filePath: path,
-                isSubmission: isScript,
+                filePath: args.WorkingDirectory,
+                isSubmission: false,
                 parseOptions: parseOptions,
                 compilationOptions: compilationOptions,
                 metadataReferences: previousProject != null ? [] : DefaultReferences,
-                projectReferences: previousProject != null ? [new ProjectReference(previousProject.Id)] : projectReferences)
-            .WithAnalyzerConfigDocuments(analyzerConfigDocuments));
+                projectReferences: previousProject != null ? [new ProjectReference(previousProject.Id)] : projectReferences));
         
-        var project = solution.GetProject(id)!;
+        var project = solution.GetProject(projectId)!;
         
-        if (!isScript && GetUsings(project) is { Length: > 0 } usings)
-        {
-            project = project.AddDocument("RoslynPadGeneratedUsings", usings).Project;
-        }
+        if (GetUsings(project) is { Length: > 0 } usings)
+            project = project.AddDocument("Usings.g.cs", usings).Project;
 
         return project;
 
         static string GetUsings(Project project)
         {
             if (project.CompilationOptions is CSharpCompilationOptions options)
-            {
-                return string.Join(" ", options.Usings.Select(i => $"global using {i};"));
-            }
+                return String.Join(" ", options.Usings.Select(i => $"global using {i};"));
 
-            return string.Empty;
+            return String.Empty;
         }
     }
 }
