7321b316edb81594a43fa7139770582e90a89eda
jkachhad <jalpeshk@gmail.com>
2025-10-17 19:08:23 -0700

    Refactor ComponentGrid and RegionDocument for improved layout and performance

--- Source/Kesmai.WorldForge/UI/Controls/ComponentGrid.xaml.cs
+++ Source/Kesmai.WorldForge/UI/Controls/ComponentGrid.xaml.cs
@@ -6,28 +6,8 @@ namespace Kesmai.WorldForge.UI;
 
 public partial class ComponentGrid : ListBox
 {
-	private UniformGridPanel _uniformGridPanel;
-	
 	public ComponentGrid()
 	{
 		InitializeComponent();
-	}
-	
-	private void OnSizeChanged(object sender, SizeChangedEventArgs args)
-	{
-		if (_uniformGridPanel is null)
-			return;
-		
-		if (args.WidthChanged)
-			_uniformGridPanel.Columns = (int)(args.NewSize.Width / 100);
-        
-		if (args.HeightChanged)
-			_uniformGridPanel.Rows = (int)(args.NewSize.Height / 120);
-	}
-
-	private void uniformGridPanel_OnLoaded(object sender, RoutedEventArgs e)
-	{
-		if (sender is UniformGridPanel uniformGridPanel)
-			_uniformGridPanel = uniformGridPanel;
 	}
 }
