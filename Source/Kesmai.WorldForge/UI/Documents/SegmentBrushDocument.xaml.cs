9b40f3debca6040e35def46b56981efbe1c1260b
jkachhad <jalpeshk@gmail.com>
2025-10-24 16:42:13 -0700

    Add SegmentBrushDocument support to ApplicationWindow and create SegmentBrushDocument user control

---
+++ Source/Kesmai.WorldForge/UI/Documents/SegmentBrushDocument.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Kesmai.WorldForge.UI.Documents;
+
+public partial class SegmentBrushDocument : UserControl
+{
+	public SegmentBrushDocument()
+	{
+		InitializeComponent();
+	}
+}
