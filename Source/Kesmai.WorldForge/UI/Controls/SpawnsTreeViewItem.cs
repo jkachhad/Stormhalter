e85fd6ce72b989596c4352bd17ce24bb13cf2bc6
jkachhad <jalpeshk@gmail.com>
2025-11-18 14:24:30 -0800

    Implement drag-and-drop functionality for SpawnsTreeViewItem with visual feedback

--- Source/Kesmai.WorldForge/UI/Controls/SpawnsTreeViewItem.cs
+++ Source/Kesmai.WorldForge/UI/Controls/SpawnsTreeViewItem.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using CommonServiceLocator;
@@ -14,11 +16,19 @@ namespace Kesmai.WorldForge.UI;
 
 internal sealed class SpawnsTreeViewItem : TreeViewItem, IDisposable
 {
+    private const string DragFormat = "Kesmai.WorldForge.SpawnsTreeViewItem.Spawn";
+
     private readonly Segment _segment;
     private readonly Dictionary<SegmentSpawner, SegmentTreeViewItem> _spawnItems = new();
     private readonly Dictionary<int, TreeViewItem> _regionNodes = new();
+    private readonly Dictionary<TreeViewItem, int> _regionLookup = new();
     
     private int _nextSpawnerId;
+    
+    private Point? _dragStartPoint;
+    private SegmentTreeViewItem _dragSourceItem;
+    private TreeViewItem _currentDropTarget;
+    private DropHighlightAdorner _currentDropAdorner;
 
     public SpawnsTreeViewItem(Segment segment, object header)
     {
@@ -30,6 +40,8 @@ internal sealed class SpawnsTreeViewItem : TreeViewItem, IDisposable
         ContextMenu = new ContextMenu();
         ContextMenu.AddItem("Add Location Spawner", "Add.png", (s, e) => AddLocationSpawner());
         ContextMenu.AddItem("Add Region Spawner", "Add.png", (s, e) => AddRegionSpawner());
+        
+        AttachDropTarget(this);
 
         _nextSpawnerId = 0;
 
@@ -68,9 +80,11 @@ internal sealed class SpawnsTreeViewItem : TreeViewItem, IDisposable
         WeakReferenceMessenger.Default.UnregisterAll(this);
         
         Items.Clear();
+        SetDropTarget(null);
         
         _spawnItems.Clear();
         _regionNodes.Clear();
+        _regionLookup.Clear();
     }
 
     private SegmentTreeViewItem CreateSpawnItem(SegmentSpawner spawner)
@@ -94,6 +108,11 @@ internal sealed class SpawnsTreeViewItem : TreeViewItem, IDisposable
         spawnerItem.ContextMenu.AddItem("Rename", "Rename.png", (s, e) => spawnerItem.Rename());
         spawnerItem.ContextMenu.AddItem("Duplicate", "Copy.png", (s, e) => spawner.Copy(_segment));
         spawnerItem.ContextMenu.AddItem("Delete", "Delete.png", (s, e) => RemoveSpawner(spawner));
+
+        AttachDropTarget(spawnerItem);
+        spawnerItem.PreviewMouseLeftButtonDown += OnSpawnPreviewMouseLeftButtonDown;
+        spawnerItem.PreviewMouseMove += OnSpawnPreviewMouseMove;
+        spawnerItem.PreviewMouseLeftButtonUp += OnSpawnPreviewMouseLeftButtonUp;
 
         _spawnItems.Add(spawner, spawnerItem);
         
@@ -134,6 +153,9 @@ internal sealed class SpawnsTreeViewItem : TreeViewItem, IDisposable
         node.ContextMenu = new ContextMenu();
         node.ContextMenu.AddItem("Add Location Spawner", "Add.png", (s, e) => AddLocationSpawner(regionId));
         node.ContextMenu.AddItem("Add Region Spawner", "Add.png", (s, e) => AddRegionSpawner(regionId));
+
+        AttachDropTarget(node);
+        _regionLookup[node] = regionId;
 
         Items.Add(node);
         _regionNodes.Add(regionId, node);
@@ -179,6 +201,209 @@ internal sealed class SpawnsTreeViewItem : TreeViewItem, IDisposable
                 _segment.Spawns.Region.Remove(regionSpawner);
                 break;
         }
+    }
+
+    private void AttachDropTarget(TreeViewItem treeViewItem)
+    {
+        treeViewItem.AllowDrop = true;
+
+        treeViewItem.PreviewDragOver += OnPreviewDrag;
+        treeViewItem.DragLeave += OnDragLeave;
+        treeViewItem.Drop += OnDrop;
+    }
+
+    private void OnPreviewDrag(object sender, DragEventArgs args)
+    {
+        if (args.Source is not UIElement dragControl)
+            return;
+
+        args.Handled = true;
+
+        if (!TryGetDraggedSpawner(args.Data, out _))
+        {
+            args.Effects = DragDropEffects.None;
+            SetDropTarget(null);
+            return;
+        }
+
+        var dragAncestor = dragControl.FindAncestor<TreeViewItem>();
+
+        if (dragAncestor is null || ReferenceEquals(dragAncestor, _dragSourceItem))
+        {
+            args.Effects = DragDropEffects.None;
+            SetDropTarget(null);
+            return;
+        }
+
+        var targetRegion = GetDropRegionFor(dragAncestor);
+
+        if (targetRegion is null)
+        {
+            args.Effects = DragDropEffects.None;
+            SetDropTarget(null);
+            return;
+        }
+
+        args.Effects = DragDropEffects.Move;
+        SetDropTarget(dragAncestor);
+    }
+
+    private void OnDragLeave(object sender, DragEventArgs args)
+    {
+        args.Handled = true;
+
+        if (!TryGetDraggedSpawner(args.Data, out _))
+            return;
+
+        if (sender is TreeViewItem treeViewItem && ReferenceEquals(treeViewItem, _currentDropTarget))
+            SetDropTarget(null);
+    }
+
+    private void OnDrop(object sender, DragEventArgs args)
+    {
+        args.Handled = true;
+        SetDropTarget(null);
+
+        if (!TryGetDraggedSpawner(args.Data, out var spawner))
+            return;
+
+        if (sender is not TreeViewItem treeViewItem)
+            return;
+
+        var targetRegion = GetDropRegionFor(treeViewItem);
+
+        if (targetRegion is null)
+            return;
+
+        var currentRegion = GetRegionValue(spawner);
+
+        if (currentRegion.HasValue && currentRegion.Value == targetRegion.Value)
+            return;
+
+        switch (spawner)
+        {
+            case LocationSegmentSpawner locationSpawner:
+                locationSpawner.Region = targetRegion.Value;
+                break;
+            case RegionSegmentSpawner regionSpawner:
+                regionSpawner.Region = targetRegion.Value;
+                break;
+        }
+    }
+
+    private static bool TryGetDraggedSpawner(IDataObject data, out SegmentSpawner spawner)
+    {
+        if (data.GetDataPresent(DragFormat) && data.GetData(DragFormat) is SegmentSpawner segmentSpawner)
+        {
+            spawner = segmentSpawner;
+            return true;
+        }
+
+        spawner = null!;
+        return false;
+    }
+
+    private void SetDropTarget(TreeViewItem? treeViewItem)
+    {
+        if (ReferenceEquals(_currentDropTarget, treeViewItem))
+            return;
+
+        RemoveCurrentDropAdorner();
+
+        _currentDropTarget = treeViewItem;
+
+        if (_currentDropTarget is null)
+            return;
+
+        _currentDropTarget.ApplyTemplate();
+
+        var layer = AdornerLayer.GetAdornerLayer(_currentDropTarget);
+
+        if (layer is null)
+        {
+            _currentDropTarget = null;
+            return;
+        }
+
+        _currentDropAdorner = new DropHighlightAdorner(_currentDropTarget);
+        layer.Add(_currentDropAdorner);
+    }
+
+    private void RemoveCurrentDropAdorner()
+    {
+        if (_currentDropAdorner is null)
+            return;
+
+        var layer = AdornerLayer.GetAdornerLayer(_currentDropAdorner.AdornedElement);
+        layer?.Remove(_currentDropAdorner);
+        _currentDropAdorner = null;
+    }
+
+    private int? GetDropRegionFor(TreeViewItem treeViewItem)
+    {
+        if (ReferenceEquals(treeViewItem, this))
+            return null;
+
+        if (_regionLookup.TryGetValue(treeViewItem, out var regionId))
+            return regionId;
+
+        if (treeViewItem is SegmentTreeViewItem segmentItem && segmentItem.Tag is SegmentSpawner spawner)
+            return GetRegionValue(spawner);
+
+        return null;
+    }
+
+    private static int? GetRegionValue(SegmentSpawner spawner)
+    {
+        return spawner switch
+        {
+            LocationSegmentSpawner locationSpawner => locationSpawner.Region,
+            RegionSegmentSpawner regionSpawner => regionSpawner.Region,
+            _ => null
+        };
+    }
+
+    private void OnSpawnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs args)
+    {
+        _dragStartPoint = args.GetPosition(this);
+        _dragSourceItem = sender as SegmentTreeViewItem;
+    }
+
+    private void OnSpawnPreviewMouseMove(object sender, MouseEventArgs args)
+    {
+        if (_dragStartPoint is null)
+            return;
+
+        if (args.LeftButton != MouseButtonState.Pressed)
+        {
+            ResetDrag();
+            return;
+        }
+
+        if (!ReferenceEquals(sender, _dragSourceItem))
+            return;
+
+        var currentPosition = args.GetPosition(this);
+
+        if (Math.Abs(currentPosition.X - _dragStartPoint.Value.X) < SystemParameters.MinimumHorizontalDragDistance &&
+            Math.Abs(currentPosition.Y - _dragStartPoint.Value.Y) < SystemParameters.MinimumVerticalDragDistance)
+            return;
+
+        if (_dragSourceItem?.Tag is SegmentSpawner spawner)
+            DragDrop.DoDragDrop(_dragSourceItem, new DataObject(DragFormat, spawner), DragDropEffects.Move);
+
+        ResetDrag();
+    }
+
+    private void OnSpawnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs args)
+    {
+        ResetDrag();
+    }
+
+    private void ResetDrag()
+    {
+        _dragStartPoint = null;
+        _dragSourceItem = null;
     }
 
     private static object CreateHeader(string name, string icon)
